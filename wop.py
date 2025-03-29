ea6c1e7d6d5732a8ca5423cc4c32f274695275f9
Administrator <kyjemail@qq.com>
2025-03-29 19:21:32 +0800

    0.1版本

---
+++ wop.py
@@ -0,0 +1,39 @@
+import tkinter as tk
+from tkinter import messagebox
+import os
+
+ve = "Beta V0.1"
+def main():
+
+    # 窗口设置
+    root = tk.Tk()
+    root.geometry("800x500-200-100")
+    root.title(f"Wop {ve}")
+
+    # 添加页面Frame
+    page = tk.Frame(root)
+
+    # 添加Menu
+    main_menu = tk.Menu(page)
+
+    # 添加HomeFrame
+    home_frame = tk.Frame()
+    # 默认显示
+    home_frame.pack()
+
+    # 标题
+    title_home = tk.Label(home_frame, text=f"Welcome ! {os.getlogin()}", font=10)
+    title_home.pack()
+
+    # 添加Home栏
+    main_menu.add_command(label="Home")
+
+
+    # 显示Menu
+    root.config(menu=main_menu)
+
+    # 程序主循环
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
---
+++ writedata.py
@@ -0,0 +1,14 @@
+import os
+
+
+# 本文件进行文件读取和写入
+
+# 获取
+user = os.path.expanduser("~")
+folder = os.path.join(user, ".wop")
+
+# 日志
+def log(logs: str):
+
+
+def new_wop():
